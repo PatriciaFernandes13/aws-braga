9eacea34128da380a8e504dd4bb5bd24a7c28e12
Patrícia Fernandes <tita1099@gmail.com>
2025-10-28 11:59:34 +0000

    Update base_de_dados.py

--- base_de_dados.py
+++ base_de_dados.py
@@ -3,7 +3,7 @@ import sys
 v1= sys.argv[1]
 v2= sys.argv[2]
 v3= sys.argv[3]
-print("Os dados do usuário são:")
+print("Os dados do utilizador são:")
 print("O servidor é:", v1)
 print("O utilizador é:", v2)
 print("A palavra passe é:", v3)
